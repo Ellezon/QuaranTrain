2dd73ebf0f5da5c8d01561055a0bcaa0b6641654
Ellezon <michelle.falzon@authenticgaming.com>
2020-04-03 20:59:41 +0200

    Initial Commit

---
+++ webpack.dev.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: ['babel-polyfill', './src/main.js'],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './src', // where dev server will look for static files, not compiled
+        publicPath: '/', //relative path to output path where  devserver will look for compiled files
+        port: 3001,
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src') // shortcut to reference src folder from anywhere
+        }
+    },
+    module: {
+        rules: [
+            { // config for es6 jsx
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            { // config for sass compilation
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader',
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            { // config for images
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'images',
+                        }
+                    }
+                ],
+            },
+            { // config for fonts
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'fonts',
+                        }
+                    }
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ // plugin for inserting scripts into html
+            chunks: ['main'],
+            template: "./src/index.html",
+            filename: "index.html",
+            title: "QuaranTrain",
+        }),
+        new MiniCssExtractPlugin({ // plugin for controlling how compiled css will be outputted and named
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+    ]
+};
+
