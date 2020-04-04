6801c999e1724f73f760a52141fa61f6bbb0cc36
Ellezon <michelle.falzon@authenticgaming.com>
2020-04-04 23:09:07 +0200

    Refactor

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import * as authFns from '@/utils/authentication.util.js';
 
-
-const Header = () => {
+const Header = ({user}) => {
     return (
         <div className='page-header'>
           <section className='logo'>
               <a href='#'>QuaranTrain</a>
+              {user && <button onClick={() => authFns.googleSignOut()}>Log Out</button>}
           </section>
         </div>
     )
