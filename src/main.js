ed2607d64bcf4355e08fea3007ac808c2068026a
Ellezon <michelle.falzon@authenticgaming.com>
2020-04-04 13:07:36 +0200

    COV-2: Firebase database and authentication

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/components/App.js';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) { // enables hot module replacement if plugin is installed
-    module.hot.accept();
+  module.hot.accept();
 }
-
-
