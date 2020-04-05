9bd756e0e4d3a8f672afea496176e9816f2d926f
Ricardo Gomes <ricardo.gomes@authenticgaming.com>
2020-04-05 15:09:50 +0200

    feat(COVID-17): Designed first screens

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -34,35 +34,20 @@ class RegisterForm extends React.Component {
 
     render() {
         const { errorMsg } = this.state;
-        const { userType } = this.props;
+        const { userType, goBack } = this.props;
 
         const isStudentToggleClasses = classNames('toggle', { 'is-selected': userType === 'isStudent' });
         const isTrainerToggleClasses = classNames('toggle', { 'is-selected': userType === 'isTrainer' });
         const errorClasses = classNames('error', { 'is-visible': errorMsg });
         return (
-            <div className='login-page content'>
+            <div className='login-page'>
                 <form onSubmit={(event) => this.handleSubmit(event)}>
-                    <div>
-                        <label htmlFor="firstName">First Name</label>
-                        <Field name="firstName" component="input" type="text" />
-                    </div>
-                    <div>
-                        <label htmlFor="lastName">Last Name</label>
-                        <Field name="lastName" component="input" type="text" />
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <Field name="email" component="input" type="email" />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <Field name="password" component="input" type="password" />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Confirm password</label>
-                        <Field name="passwordConfirmation" component="input" type="password" />
-                    </div>
-                    <div>
+                    <Field className='input' placeholder='First Name' name="firstName" component="input" type="text" />
+                    <Field className='input' placeholder='Last Name' name="lastName" component="input" type="text" />
+                    <Field className='input' placeholder='E-mail' name="email" component="input" type="email" />
+                    <Field className='input' placeholder='Password' name="password" component="input" type="password" />
+                    <Field className='input' placeholder='Confirm password' name="passwordConfirmation" component="input" type="password" />
+                    <div className='toggles'>
                         <label className={isStudentToggleClasses}>
                             <Field name="userType" component="input" type="radio" value="isStudent" defaultChecked={true}/> Student
                         </label>
@@ -70,14 +55,11 @@ class RegisterForm extends React.Component {
                             <Field name="userType" component="input" type="radio" value="isTrainer" /> Trainer
                         </label>
                     </div>
-                    <button type="submit">Sign Up</button>
+                    <button className='button is-primary'  type="submit">Sign Up</button>
                 </form>
-                <div className="small-text">
-                    <p>
-                        Already have an account?
-                        <a href="#">Sign in</a>
-                    </p>
-                    <a href="/">Go back</a>
+                <div className="small-text" onClick={() => goBack()}>
+                    <p>Already have an account?</p>
+                    <a href="#">Go back</a>
                 </div>
 
                 <div className={errorClasses}>
